c5cb69bacc9c41a5a61fcba5cf13d254d04ecd71
cryptomanz1 <85495047+cryptomanz1@users.noreply.github.com>
2023-09-11 09:12:43 +0300

    Added Circle Shape for gifs

--- app/src/main/java/com/example/giphyapp/gifs/GifsScreen.kt
+++ app/src/main/java/com/example/giphyapp/gifs/GifsScreen.kt
@@ -1,12 +1,14 @@
 package com.example.giphyapp.gifs
 
 import android.os.Build.VERSION.SDK_INT
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -34,6 +37,7 @@ fun GifsScreen() {
         content = {
             val items = viewModel.viewData.observeAsState()
             val context = LocalContext.current
+            val borderWidth = 4.dp
             LazyVerticalGrid(
                 modifier = Modifier.padding(it),
                 columns = GridCells.Adaptive(128.dp)
@@ -58,8 +62,14 @@ fun GifsScreen() {
                         imageLoader = imageLoader,
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
-                            .size(180.dp)
-                            .clip(RoundedCornerShape(16.dp)))
+                            .size(150.dp)
+                            .border(
+                                BorderStroke(borderWidth, Color.DarkGray),
+                                CircleShape
+                            )
+                            .padding(borderWidth)
+                            .clip(CircleShape)
+                    )
                 }
             }
         }
